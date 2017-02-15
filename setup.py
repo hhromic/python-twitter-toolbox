a09cb869f239a96f4d33af09d0b0ff3c4bc32dd2
Hugo Hromic <hhromic@gmail.com>
2017-02-15 17:01:30 +0000

    fix installation without dependencies installed on some platforms

--- setup.py
+++ setup.py
@@ -2,27 +2,15 @@
 
 """Main setup script."""
 
+import ast
 from setuptools import setup, find_packages
-import twtoolbox.cli
 
-CONSOLE_SCRIPTS = ["%s = twtoolbox.cli:%s" % (fn.replace("_", "-"), fn)
-                   for fn in dir(twtoolbox.cli) if fn.startswith("tt_")]
+CLI_TREE = ast.parse(open('twtoolbox/cli.py').read())
+CONSOLE_SCRIPTS = ["%s = twtoolbox.cli:%s" % (fn.name.replace("_", "-"), fn.name)
+                   for fn in CLI_TREE.body
+                   if isinstance(fn, ast.FunctionDef) and fn.name.startswith('tt_')]
 
 setup(
-    name="twitter-toolbox",
     packages=find_packages(),
-    package_data={"twtoolbox": ["defaults.cfg"]},
-    version="1.0.3",
-    description="Twitter Toolbox for Python",
-    long_description="Twitter Toolbox for Python",
-    author="Hugo Hromic",
-    author_email="hhromic@gmail.com",
-    url="https://github.com/hhromic/python-twitter-toolbox",
-    download_url="https://github.com/hhromic/python-twitter-toolbox/tarball/1.0.3",
-    install_requires=["tweepy", "colorlog"],
-    keywords=["twitter", "api", "cli", "toolbox"],
-    classifiers=["Environment :: Console"],
-    license="Apache-2.0",
-    platforms=["all"],
     entry_points={"console_scripts": CONSOLE_SCRIPTS},
 )
