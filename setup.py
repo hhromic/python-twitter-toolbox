3acefa24819621c9db9d7489dbb2f1bedb43fddf
Hugo Hromic <hhromic@gmail.com>
2017-02-17 20:17:23 +0000

    reverted wrong setuptools changes and enhanced setup script

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# pylint: disable=missing-docstring
 
 """Main setup script."""
 
@@ -6,12 +7,37 @@ import ast
 from os import path
 from setuptools import setup, find_packages
 
-CLI_TREE = ast.parse(open(path.join("twtoolbox", "cli.py")).read())
-CONSOLE_SCRIPTS = ["%s = twtoolbox.cli:%s" % (fn.name.replace("_", "-"), fn.name)
-                   for fn in CLI_TREE.body
-                   if isinstance(fn, ast.FunctionDef) and fn.name.startswith('tt_')]
+VERSION = "1.2.0"
+DESCRIPTION = "Twitter Toolbox for Python"
+AUTHOR = "Hugo Hromic"
+AUTHOR_EMAIL = "hhromic@gmail.com"
+URL = "https://github.com/hhromic/python-twitter-toolbox"
+
+def read_long_description():
+    with open("README.rst") as reader:
+        return reader.read()
+
+def generate_console_scripts():
+    with open(path.join("twtoolbox", "cli.py")) as reader:
+        cli_tree = ast.parse(reader.read())
+    return ["%s = twtoolbox.cli:%s" % (fn.name.replace("_", "-"), fn.name)
+            for fn in cli_tree.body if isinstance(fn, ast.FunctionDef) and
+            fn.name.startswith('tt_')]
 
 setup(
+    name="twitter-toolbox", version=VERSION, description=DESCRIPTION,
+    author=AUTHOR, author_email=AUTHOR_EMAIL,
+    maintainer=AUTHOR, maintainer_email=AUTHOR_EMAIL,
+    url=URL, download_url=URL + "/tarball/" + VERSION,
+    requires=["tweepy", "colorlog"],
+    install_requires=["tweepy", "colorlog"],
+    provides=["twtoolbox"],
+    keywords=["twitter", "api", "cli", "toolbox"],
+    classifiers=["Environment :: Console"],
+    license="Apache-2.0",
+    platforms=["all"],
+    long_description=read_long_description(),
     packages=find_packages(),
-    entry_points={"console_scripts": CONSOLE_SCRIPTS},
+    package_data={"twtoolbox": ["defaults.cfg"]},
+    entry_points={"console_scripts": generate_console_scripts()},
 )
