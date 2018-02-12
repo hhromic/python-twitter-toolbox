b51c093bc26cbdab6619353ad4981e5781810076
Hugo Hromic <hhromic@gmail.com>
2018-02-12 15:02:10 +0000

    prepare for new version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ import ast
 from os import path
 from setuptools import setup, find_packages
 
-VERSION = "1.2.1"
+VERSION = "1.2.2"
 DESCRIPTION = "Twitter Toolbox for Python"
 AUTHOR = "Hugo Hromic"
 AUTHOR_EMAIL = "hhromic@gmail.com"
