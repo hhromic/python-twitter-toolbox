de35932199a8dc2ea154a72a74727bfbbf3ecb7f
Hugo Hromic <hhromic@gmail.com>
2018-12-12 15:27:41 +0000

    improve setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,22 @@
-#!/usr/bin/env python
-# pylint: disable=missing-docstring
-
 """Main setup script."""
 
 import ast
 from os import path
 from setuptools import setup, find_packages
 
-VERSION = "1.2.3"
+NAME = "twitter-toolbox"
+VERSION = "1.2.4"
 DESCRIPTION = "Twitter Toolbox for Python"
 AUTHOR = "Hugo Hromic"
 AUTHOR_EMAIL = "hhromic@gmail.com"
 URL = "https://github.com/hhromic/python-twitter-toolbox"
+DOWNLOAD_URL = URL + "/tarball/" + VERSION
 
-def read_long_description():
-    with open("README.rst") as reader:
+def _read_file(filename):
+    with open(filename) as reader:
         return reader.read()
 
-def generate_console_scripts():
+def _gen_console_scripts():
     with open(path.join("twtoolbox", "cli.py")) as reader:
         cli_tree = ast.parse(reader.read())
     return ["%s = twtoolbox.cli:%s" % (fn.name.replace("_", "-"), fn.name)
@@ -25,10 +24,10 @@ def generate_console_scripts():
             fn.name.startswith('tt_')]
 
 setup(
-    name="twitter-toolbox", version=VERSION, description=DESCRIPTION,
+    name=NAME, version=VERSION, description=DESCRIPTION,
     author=AUTHOR, author_email=AUTHOR_EMAIL,
     maintainer=AUTHOR, maintainer_email=AUTHOR_EMAIL,
-    url=URL, download_url=URL + "/tarball/" + VERSION,
+    url=URL, download_url=DOWNLOAD_URL,
     requires=["tweepy", "colorlog"],
     install_requires=["tweepy", "colorlog"],
     provides=["twtoolbox"],
@@ -36,9 +35,9 @@ setup(
     classifiers=["Environment :: Console"],
     license="Apache-2.0",
     platforms=["all"],
-    long_description=read_long_description(),
-    long_description_content_type="text/x-rst",
+    long_description=_read_file("README.md"),
+    long_description_content_type="text/markdown",
     packages=find_packages(),
     package_data={"twtoolbox": ["defaults.cfg"]},
-    entry_points={"console_scripts": generate_console_scripts()},
+    entry_points={"console_scripts": _gen_console_scripts()},
 )
